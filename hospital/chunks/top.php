c24082dedad03b2d4e7b36e719db372bf52c56c6
Sachin Kekarjawalekar <kekarjawalekarsachin1@gmail.com>
2020-04-23 07:34:01 +0530

    Update top.php

--- hospital/chunks/top.php
+++ hospital/chunks/top.php
@@ -67,7 +67,7 @@ $con = $db->con();
                     <li>
                         <h1 class="logo-wrapper">
                             <a href="./" class="brand-logo darken-1">
-                                <img src="../assets/logo/ndma.png" align="center" alt="Logo">
+                                <img src="../assets/logo/hics.png" align="center" alt="Logo">
                                 <span class="logo-text hide-on-med-and-down">Doctor's Dashboard</span>
                             </a>
                         </h1>
